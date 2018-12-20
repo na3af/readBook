56d8ebbc3357959d4b4ec06b23308c1fa11313c7
Nawaf <nawaf-alhomaid@hotmail.com>
2018-12-20 11:26:39 +0300

    sntaks error

--- __init__.py
+++ __init__.py
@@ -61,7 +61,7 @@ def gconnect():
     code = request.data
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets(path+’/client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets(path+'/client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
